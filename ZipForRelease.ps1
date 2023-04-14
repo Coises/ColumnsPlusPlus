b966d5146c1630ccd5c003e8b332abd7fa86b555
Coises <git@coises.com>
2023-04-14 14:15:12 -0700

    Include SHA-256 hashes in ZipForRelease output.

--- ZipForRelease.ps1
+++ ZipForRelease.ps1
@@ -1,5 +1,11 @@
-$version = (Get-Command "vs.proj\bin\Win32\Release\ColumnsPlusPlus.dll").FileVersionInfo.ProductVersion
-Compress-Archive -DestinationPath ColumnsPlusPlus-$version-x86.zip -Path "vs.proj\bin\Win32\Release\ColumnsPlusPlus.dll","CHANGELOG.md","help.htm","LICENSE.txt","source.txt"
-$version = (Get-Command "vs.proj\bin\x64\Release\ColumnsPlusPlus.dll").FileVersionInfo.ProductVersion
-Compress-Archive -DestinationPath ColumnsPlusPlus-$version-x64.zip -Path "vs.proj\bin\x64\Release\ColumnsPlusPlus.dll","CHANGELOG.md","help.htm","LICENSE.txt","source.txt"
+$x86Version = (Get-Command "vs.proj\bin\Win32\Release\ColumnsPlusPlus.dll").FileVersionInfo.ProductVersion
+Compress-Archive -DestinationPath "ColumnsPlusPlus-$x86Version-x86.zip" -Path "vs.proj\bin\Win32\Release\ColumnsPlusPlus.dll","CHANGELOG.md","help.htm","LICENSE.txt","source.txt"
+$x64Version = (Get-Command "vs.proj\bin\x64\Release\ColumnsPlusPlus.dll").FileVersionInfo.ProductVersion
+Compress-Archive -DestinationPath "ColumnsPlusPlus-$x64Version-x64.zip" -Path "vs.proj\bin\x64\Release\ColumnsPlusPlus.dll","CHANGELOG.md","help.htm","LICENSE.txt","source.txt"
+$x86Hash = (Get-FileHash "ColumnsPlusPlus-$x86Version-x86.zip" -Algorithm SHA256).Hash
+$x64Hash = (Get-FileHash "ColumnsPlusPlus-$x64Version-x64.zip"   -Algorithm SHA256).Hash
+$Hashes =  "SHA-256 Hash (ColumnsPlusPlus-$x64Version-x64.zip): " + $x64Hash + "`nSHA-256 Hash (ColumnsPlusPlus-$x86Version-x86.zip): " + $x86Hash
+Write-Output $Hashes
+Write-Output ""
+Set-Clipboard -Value $Hashes
 Read-Host -Prompt "Press Enter to exit"
